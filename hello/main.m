436deee2d3e99b4c93e28c587ba6ffc621b9b338
Your Name <your_email@your-domain.com>
2015-09-09 22:10:46 +0300

    Initial Commit

---
+++ hello/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello
+//
+//  Created by Alexandra on 09.09.15.
+//  Copyright (c) 2015 Alexandra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
