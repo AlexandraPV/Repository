b9f4ae6faa0b18a1906c7e01905f408e289d1e62
Your Name <your_email@your-domain.com>
2015-09-10 20:40:37 +0300

    first commit

---
+++ courses/prog_base/tasks/hello_world/hello.c
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hello.c
+//
+//  Created by Alexandra on 09.09.15.
+//  Copyright (c) 2015 Alexandra. All rights reserved.
+//
+
+//C hello world example
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
