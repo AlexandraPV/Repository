3597fde74a0f78a479c51817f8f129656fad608a
AlexandraPV <alisandra.pv@gmail.com>
2015-10-18 13:36:32 +0300

    Update repr.c

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -4,12 +4,17 @@
 
 double long2double();
 int main(){
-    long num;                //num-first number
-    int E=0, S, x, y;        // E- exponenta, S - sign
-    double F=0, V;           //F-mantisa, V- final number
+    long num;
+    printf("%.325lf",long2double(num));
+}
+
+double long2double() {
+    long num;                    //num-first number
+    char S, x, y;               // S - sign
+    double F=0,E=0, V;         //F-mantisa, V- final number, E- exponenta
     
-    printf("Long number:");
-    scanf("%ld", &num);
+   printf("Long number:");
+   scanf("%ld", &num);
     
     S=(num>>63)&1;
     for (y=52; y<63; ++y) {
@@ -20,37 +25,37 @@ int main(){
         F+=((num>>x)&1)*(pow(2, (x-52)));
     }
     
-
+    
     if (E==2047 && F!=0)
-        printf("V=Nan");
+        return NAN;
     
     else
-    if (E==2047 && F==0 && S==1)
-        printf("V= -Infinity");
+        if (E==2047 && F==0 && S==1)
+            return  -INFINITY;
     
     else
-    if (E==2047 && F==0 && S==0)
-        printf("V= Infinity");
+        if (E==2047 && F==0 && S==0)
+            return INFINITY;
     
     else
     if ( E>0 && E<2047)
     {
         V=pow((-1),S)*pow(2,E-1023)*(1+F);
-        printf("%.325f",V);
+        return V;
     }
     
     else
     if (E==0 && F!=0 ) {
         V=pow((-1), S)*pow(2, (-1022))*(F);
-        printf("%.325f",V);
+        return V;
     }
     
     else
-    if (E==0 && F==0 && S==1)
-        printf("V=-0");
+        if (E==0 && F==0 && S==1)
+            return -0;
     
     else
-    if (E==0 && F==0 && S==0)
-        printf("V=0");
-    
+        if (E==0 && F==0 && S==0)
+            return 0;
+    return num;
 }
