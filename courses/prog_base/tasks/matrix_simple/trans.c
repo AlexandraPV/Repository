5d23a37f7857dd22d0adb25b30899bf91b1f74b1
AlexandraPV <alisandra.pv@gmail.com>
2015-10-24 10:48:12 +0300

    Update trans.c

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -27,11 +27,11 @@ void flipV(int mat[4][4])
     }
 }
 
- void transposSide(int mat[4][4])
+ void transposMain(int mat[4][4])
  {
   for(int i=0; i<4; i++)
     {
         for(int j=0; j<4; j++)
-           mat [4-1-j][4-1-i]= mat[i][j];
+           mat [j][i]= mat[i][j];
     }
   }
