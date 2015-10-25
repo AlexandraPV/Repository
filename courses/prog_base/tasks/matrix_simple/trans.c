869e6757fe572119345249e3058c8c9599fca1f8
AlexandraPV <alisandra.pv@gmail.com>
2015-10-25 21:39:09 +0200

    Update trans.c

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -11,21 +11,49 @@ void fillRand(int mat[4][4])
 
 void rotateCCW180(int mat[4][4])
 {
-    for(int i=0; i<4; i++)
+    
+    int mat1[4][4];
+    for(int a=0; a<4; a++)
     {
-        for(int j=0; j<4; j++)
-            mat [4-1-i][4-1-j] = mat[i][j];
+        for(int b=0; b<4; b++)
+            mat1[a][b] = mat[a][b];
+    }
+    
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        mat[i][j] = mat1[4-1-i][4-1-j];
     }
 }
 
-void flipV(int mat[4][4])
+
+
+
+
+void flipV( int mat[4][4])
 {
-    for(int i=0; i<4; i++)
-    {
-        for(int j=0; j<4; j++)
-            mat [4-1-i][j]=mat[i][j];
+    int mat1[4][4];
+    int mat2[4][4];
+    
+    
+        for (int a=0;a<4;a++){
+            for (int b=0;b<4;b++)
+                mat1[a][b]=mat[a][b];
+        }
+       for (int i=0;i<4;i++){
+            for (int j=0;j<4;j++){
+                mat2[i][j]=mat1[4-1-i][4-1-j];
+                
+            }
+        }
+    
+    
+    for (int i=0;i<4;i++){
+        for (int j=0;j<4;j++)
+            mat[i][j]=mat2[4-1-i][j];
     }
 }
+
 
  void transposMain(int mat[4][4])
  {
