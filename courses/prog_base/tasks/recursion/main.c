db307f299824e15a8278957623906c3d3712fb32
AlexandraPV <alisandra.pv@gmail.com>
2015-12-13 19:41:45 +0200

    Update main.c

--- courses/prog_base/tasks/recursion/main.c
+++ courses/prog_base/tasks/recursion/main.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+int isHexadecimal (char ch) {
+    return (isdigit (ch) || ((ch >= 'a') && (ch <= 'f')) || ((ch >= 'A') && (ch <= 'F')));
+}
 
 char Recurs(char *str, int i){
-    if (i== (strlen(str)-1) )
+    if (i== (strlen(str)-1))
         return (tolower(str[0])<tolower(str[i])) ? str[0] : str[i];
     else
         return Recurs(str, i + 1);
@@ -22,12 +24,14 @@ int Function (char symbol){
 
 int main()
 {
+    int i;
     char *str=malloc(100*sizeof(char));
     printf("Enter string:");
     scanf("%s", str);
-    
-    
-    printf("Smaller value: %d", Function (Recurs(str, 0)));
+    if (isHexadecimal(str[0]) && (isHexadecimal(str[strlen(str) - 1])))
+        printf("Smaller value: %d", Function (Recurs(str, 0)));
+    else
+        printf("Error! First and last characters are not valid hexadecimal numbers");
     free(str);
     return 0;
     
