937bb8d494804e1ece3f30543989c09b88b21b4a
AlexandraPV <alisandra.pv@gmail.com>
2016-03-01 09:11:52 +0200

    Update student.c

--- courses/prog_base_2/tasks/module/student.c
+++ courses/prog_base_2/tasks/module/student.c
@@ -1,31 +1,35 @@
 #include "student.h"
 #include <stdlib.h>
+#include <strings.h>
 #define QUANTITY_STUDENTS 10
 #define MAX_SIZE_OF_NAME 20
 
-struct STUDENT {
-    char name[MAX_SIZE_OF_NAME]; 
+struct student_s {
+    char name[MAX_SIZE_OF_NAME]; //фамилия
+    //int * mas;
+    float ball;	//средний
+};
+struct list_s {
+    struct student_s * student;
     int size;
-    int * mas;
-    float ball;	
 };
 
-list_t *list_creat (){
+list_t * list_creat (int size){
     
     list_t * std = malloc(sizeof(list_t));
-    std->mas = malloc(100*sizeof(int));
+    std->student = malloc(size * sizeof(student_t));
     std->size = 0;
     return std;
 }
 
 
- enum RESULTS add_elem(list_t * self,char last_name, int index, float average_score){
-    if (index < self->size){
+enum RESULTS add_elem(list_t * self,char last_name[MAX_SIZE_OF_NAME], int index, float average_score){
+    if (index <= self->size && index >= 0){
         for (int i = self->size - 1; i >= index; i--){
-            self->mas[i+1] = self->mas[i];
+            self->student[i+1] = self->student[i];
         }
-        self->name[index] = last_name;
-        self->ball=average_score;
+        strcpy(self->student[index].name, last_name);
+        self->student[index].ball = average_score;
         self->size++;
         return SUCCESS;
     }
@@ -34,39 +38,36 @@ list_t *list_creat (){
 }
 
 enum RESULTS delete_elem(list_t * self, int index){
-    if (index < self->size){
+    if (index < self->size && index >= 0){
         for (int i = index ; i < self->size; i++){
-            self->mas[i]=self->mas[i+1];
+            self->student[i] = self->student[i + 1];
          }
+        self->size--;
         return SUCCESS;
     }else return NO_INDEX;
 }
 
 
 int size_of_list(list_t * self){
-    int length;
-    for(int i=0; i<self->size; i++){
-        length++;
-    }
-    return length;
+    return self->size;
 }
 
 int student_max_mark(list_t * self ){
     float max=0;
-    for(int i=0; i<self->size; i++){
-        if( self->ball > max){
-            max = self->ball;
+    for(int i=0; i < self->size; i++){
+        if( self->student[i].ball > max){
+            max = self->student[i].ball;
         }
     }
     return max;
 }
 
 
-int student_name(list_t * self, char name){
+int student_name(list_t * self, char name[MAX_SIZE_OF_NAME]){
     for (int i=0; i<self->size; i++) {
-        if (name == self->name){
-            printf("Surname: %s", self->name);
-            printf("Score: %f", self->ball);
+        if (strcmp(name, self->student[i].name) == 0){
+            printf("Surname: %s\n", self->student[i].name);
+            printf("Score: %.2f\n", self->student[i].ball);
         }
     }
     return 0;
@@ -74,8 +75,12 @@ int student_name(list_t * self, char name){
 
 
 void print_list(list_t * self){
-    for (int i = 0; i < self->size; i++){
-        printf("%d ", self->mas[i]);
-    }
+    for (int i = 0; i < self->size; i++)
+        printf("%s %.2f\n", self->student[i].name, self->student[i].ball);
+}
 
+void list_remove(list_t ** std){
+    free((*std)->student);
+    free(*std);
+    *std = NULL;
 }
