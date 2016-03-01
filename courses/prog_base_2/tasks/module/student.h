8b05ed9a5fa13d2900f8f934768ff22b8b37df8a
AlexandraPV <alisandra.pv@gmail.com>
2016-03-01 09:12:19 +0200

    Update student.h

--- courses/prog_base_2/tasks/module/student.h
+++ courses/prog_base_2/tasks/module/student.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-typedef struct STUDENT list_t;
+typedef struct list_s list_t;
+typedef struct student_s student_t;
 
 enum RESULTS{
     LIST_IS_EMPTY,
@@ -13,14 +14,15 @@ enum RESULTS{
     EMPTY
 };
 
-list_t  *list_creat();
+list_t  *list_creat(int size);
 
-enum RESULTS add_elem(list_t *,char, int, float);    //add element to list
-enum RESULTS delete_elem(list_t *,int);     //delete element from list
+enum RESULTS add_elem(list_t *,char [], int, float);    //add element to list
+enum RESULTS delete_elem(list_t *, int);     //delete element from list
 int size_of_list(list_t *);            // size of list
 int student_max_mark(list_t *);        // find the student with the highest average score
-int student_name(list_t *, char);            //find the student (use name)
+int student_name(list_t *, char []);            //find the student (use name)
 void print_list(list_t * );
 enum RESULTS list_free(list_t *);
+void list_remove(list_t ** std);
 
 #endif /* student_h */
