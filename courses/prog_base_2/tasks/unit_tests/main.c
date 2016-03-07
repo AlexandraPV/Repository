b188db08efc13b2adb30bbd6151cce1ce12b1a66
AlexandraPV <alisandra.pv@gmail.com>
2016-03-07 13:50:14 +0200

    Create main.c

---
+++ courses/prog_base_2/tasks/unit_tests/main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>  // !
+#include <stdarg.h>  // !
+#include <stddef.h>  // !
+#include <setjmp.h>  // !
+
+#include <cmocka.h>
+
+#include "list.h"
+
+static void new_void_listAdd(void **stus){
+    list_t * group1 = list_creat(8);
+    assert_int_equal(list_add_elem(group1, "Ivanov", 0, 4),SUCCESS);
+    assert_int_equal(list_add_elem(group1, "Petrov", 10, 3.5),NO_INDEX);
+    assert_int_equal(list_remove(group1),SUCCESS);
+    
+}
+
+static void new_void_listDelete(void **stus){
+    list_t * group2 = list_creat(8);
+    assert_int_equal(list_delete_elem(group2, 9),NO_INDEX);
+    assert_int_equal(list_delete_elem(group2, 0),SUCCESS);
+    list_remove(group2);
+}
+
+static void new_void_listSizeAndMark(void **stus){
+    list_t * std = list_creat();
+    assert_int_equal(list_size(std),0);
+    list_add_elem(std, "Zhuravel", 0, 4.3);
+    list_add_elem(std, "Shannon", 1, 4.7);
+    list_add_elem(std, "Antonov", 2, 5);
+    assert_int_equal(list_size(std),3);
+    assert_int_equal(student_max_mark(std),5);
+    list_remove(std);
+}
+
+
+
+int main(void) {
+    const struct CMUnitTest tests[] =
+    {
+        cmocka_unit_test(new_void_listAdd),
+        cmocka_unit_test(new_void_listDelete),
+        cmocka_unit_test(new_void_listSizeAndMark),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
