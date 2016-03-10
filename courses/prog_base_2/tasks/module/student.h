71bc6f9bd47a3c3b9da5ba644cef6c3d4b950294
AlexandraPV <alisandra.pv@gmail.com>
2016-03-10 21:31:20 +0200

    Update student.h

--- courses/prog_base_2/tasks/module/student.h
+++ courses/prog_base_2/tasks/module/student.h
@@ -20,7 +20,7 @@ enum RESULTS list_add_elem(list_t *,char [], int, float);    //add element to li
 enum RESULTS list_delete_elem(list_t *, int);     //delete element from list
 int list_size(list_t *);            // size of list
 int list_student_max_mark(list_t *);        // find the student with the highest average score
-int list_student_name(list_t *, char []);            //find the student (use name)
+void list_student_name(list_t *, char []);            //find the student (use name)
 void list_print(list_t * );
 enum RESULTS list_free(list_t *);
 void list_remove(list_t ** std);
