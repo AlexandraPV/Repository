0d92dcda930ea2e462b818ce123c875eeb15610d
AlexandraPV <alisandra.pv@gmail.com>
2016-03-10 21:40:22 +0200

    Update main.c

--- courses/prog_base_2/tasks/unit_tests/main.c
+++ courses/prog_base_2/tasks/unit_tests/main.c
@@ -7,28 +7,31 @@
 
 #include "list.h"
 
-static void new_void_listAdd(void **stus){
+static void new_void_list_add_elem(void **stus){
     list_t * group1 = list_creat(8);
     assert_int_equal(list_add_elem(group1, "Ivanov", 0, 4),SUCCESS);
     assert_int_equal(list_add_elem(group1, "Petrov", 10, 3.5),NO_INDEX);
     assert_int_equal(list_remove(group1),SUCCESS);
+    list_remove(group1);
     
 }
 
-static void new_void_listDelete(void **stus){
+static void new_void_list_delete_elem(void **stus){
     list_t * group2 = list_creat(8);
     assert_int_equal(list_delete_elem(group2, 9),NO_INDEX);
     assert_int_equal(list_delete_elem(group2, 0),SUCCESS);
     list_remove(group2);
 }
 
-static void new_void_listSizeAndMark(void **stus){
+static void new_void_list_size_and_max_mark(void **stus){
     list_t * std = list_creat();
     assert_int_equal(list_size(std),0);
     list_add_elem(std, "Zhuravel", 0, 4.3);
     list_add_elem(std, "Shannon", 1, 4.7);
     list_add_elem(std, "Antonov", 2, 5);
     assert_int_equal(list_size(std),3);
+    assert_int_equal(student_max_mark(std),5);
+    list_add_elem(std, "Ivanov", 3, 5);
     assert_int_equal(student_max_mark(std),5);
     list_remove(std);
 }
