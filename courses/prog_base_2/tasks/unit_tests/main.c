1f694b40e60c77bf8facc88d5d9323564f2c4d98
AlexandraPV <alisandra.pv@gmail.com>
2016-03-20 12:19:00 +0200

    Update main.c

--- courses/prog_base_2/tasks/unit_tests/main.c
+++ courses/prog_base_2/tasks/unit_tests/main.c
@@ -7,7 +7,7 @@
 
 #include "list.h"
 
-static void new_void_list_add_elem(void **stus){
+static void list_add_elem__newElementAnd_index__successOrNoindexResult(void **stus){
     list_t * group1 = list_creat(8);
     assert_int_equal(list_add_elem(group1, "Ivanov", 0, 4),SUCCESS);
     assert_int_equal(list_add_elem(group1, "Petrov", 10, 3.5),NO_INDEX);
@@ -16,34 +16,51 @@ static void new_void_list_add_elem(void **stus){
     
 }
 
-static void new_void_list_delete_elem(void **stus){
+static void list_delete_elem__newElementAnd_index__successOrNoindexResult(void **stus){
     list_t * group2 = list_creat(8);
     assert_int_equal(list_delete_elem(group2, 9),NO_INDEX);
     assert_int_equal(list_delete_elem(group2, 0),SUCCESS);
     list_remove(group2);
 }
 
-static void new_void_list_size_and_max_mark(void **stus){
+static void list_max_mark__StudentStruct__max_marc(void **stus){
+    list_t * std = list_creat();
+    list_add_elem(std, "Zhuravel", 0, 4.3);
+    list_add_elem(std, "Shannon", 1, 4.7);
+    list_add_elem(std, "Antonov", 2, 5);
+    assert_int_equal(student_max_mark(std),5);
+    list_add_elem(std, "Ivanov", 3, 5);
+    assert_int_equal(student_max_mark(std),5);
+    list_remove(std);
+}
+static void list_size__ListStudent__size(void **stus){
     list_t * std = list_creat();
     assert_int_equal(list_size(std),0);
     list_add_elem(std, "Zhuravel", 0, 4.3);
     list_add_elem(std, "Shannon", 1, 4.7);
     list_add_elem(std, "Antonov", 2, 5);
     assert_int_equal(list_size(std),3);
-    assert_int_equal(student_max_mark(std),5);
     list_add_elem(std, "Ivanov", 3, 5);
-    assert_int_equal(student_max_mark(std),5);
     list_remove(std);
 }
-
-
+static void list_student_name__StudentStructAnd_name__success(void **stus){
+    list_t * std = list_creat();
+    list_add_elem(std, "Zhuravel", 0, 4.3);
+    list_add_elem(std, "Shannon", 1, 4.7);
+    list_add_elem(std, "Antonov", 2, 5);
+    assert_int_equal(list_student_name(std, "Antonov"),1);
+    assert_int_equal(list_student_name(std, "Petrov"),0);
+    list_remove(std);
+}
 
 int main(void) {
     const struct CMUnitTest tests[] =
     {
-        cmocka_unit_test(new_void_listAdd),
-        cmocka_unit_test(new_void_listDelete),
-        cmocka_unit_test(new_void_listSizeAndMark),
+        cmocka_unit_test(list_add_elem__newElementAnd_index__successOrNoindexResult),
+        cmocka_unit_test(list_delete_elem__newElementAnd_index__successOrNoindexResult),
+        cmocka_unit_test(list_max_mark__StudentStruct__max_marc),
+        cmocka_unit_test(list_size__ListStudent__size),
+        cmocka_unit_test(list_student_name__StudentStructAnd_name__success),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
