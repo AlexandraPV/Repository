ea209d647d7c9bbb8fdcd292b9e134303c26eedf
AlexandraPV <alisandra.pv@gmail.com>
2016-04-02 10:40:08 +0300

    Update Seat.h

--- courses/prog_base_2/labs/lab1/Seat.h
+++ courses/prog_base_2/labs/lab1/Seat.h
@@ -1,9 +1,16 @@
 #ifndef Seat_h
 #define Seat_h
 
+#include <stdbool.h>
 #include <stdio.h>
+
 typedef struct A SEAT;
 SEAT * seat_new (int i);
+void seat_free(SEAT *st);
 int seat_print (SEAT * seat);
+int seat_return(SEAT * seat);
+bool seat_status(SEAT * seat);
+bool seat_unReservation(SEAT * seat);
+//bool seat_unBuy(SEAT * seat);
 
 #endif /* Seat_h */
