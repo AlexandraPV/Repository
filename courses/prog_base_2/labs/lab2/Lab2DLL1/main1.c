03372814d3d287503add3383feb04ceb96678024
AlexandraPV <alisandra.pv@gmail.com>
2016-04-23 11:37:04 +0300

    Create main1.c

---
+++ courses/prog_base_2/labs/lab2/Lab2DLL1/main1.c
@@ -0,0 +1,21 @@
+#include "main1.h"
+
+void addElem(STACK *stack){
+    printf("Enter string ");
+    char *str;
+    scanf("%s", str);
+    stack_push(stack, str);
+    
+}
+
+void removeElems(STACK *stack){
+    printf("Enter count of delete elements ");
+    int count;
+    scanf("%d",&count);
+    if (count<1 || count>11)
+        printf ("Wrong count");
+    else{
+        for(int i=0; i<count; i++)
+        stack_pop(stack);
+    }
+}
