b12fefa189b97a5a8907587a5b9c91595bacb9cd
AlexandraPV <alisandra.pv@gmail.com>
2016-04-24 15:56:20 +0300

    Create db_manager.h

---
+++ courses/prog_base_2/tasks/database/db_manager.h
@@ -0,0 +1,15 @@
+#include "director.h"
+#include <sqlite3.h>
+
+typedef struct db_s db_t;
+
+db_t * db_new(const char * dbFile);
+void db_free(db_t * self);
+void table_field(sqlite3_stmt * stmt, director_t *director);
+int db_insertDirector(db_t * self, director_t * director);
+director_t * db_getDirectorById(db_t * self, int id);
+void db_updateDirector(db_t * self, director_t * director, int id);
+void db_deleteDirectorById(db_t * self, int id);
+int db_countDirectors(db_t * self);
+int db_filterDirectors(db_t * self, int K, double P, director_t * directorSet, int setMaxLen);
+
