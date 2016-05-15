ba1e7e943ddab1972d5002e7ae2e0b281217aefb
AlexandraPV <alisandra.pv@gmail.com>
2016-05-15 15:43:52 +0300

    Create director.h

---
+++ courses/prog_base_2/tasks/windows/director.h
@@ -0,0 +1,15 @@
+#ifndef director_h
+#define director_h
+
+#include <stdio.h>
+#include <windows.h>
+#include <CommCtrl.h>
+#include <math.h>
+
+char * randName();
+char * randSurname();
+char * randYears();
+char * randBudget();
+char * randBirthDate();
+
+#endif /* director_h */
