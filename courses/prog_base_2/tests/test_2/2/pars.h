e0065cc098f5f50eda95d0ef4493b86ea7c0853e
AlexandraPV <alisandra.pv@gmail.com>
2016-05-28 14:09:07 +0300

    Add files via upload

---
+++ courses/prog_base_2/tests/test_2/2/pars.h
@@ -0,0 +1,16 @@
+//
+//  pars.h
+//  2
+//
+//  Created by Alexandra on 28.05.16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
+#ifndef pars_h
+#define pars_h
+
+#include <stdio.h>
+#include "Director.h"
+void pars(direct ** dir);
+
+#endif /* pars_h */
