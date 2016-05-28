7bf27ec48c67d22ca92722496d88d48e2910e935
AlexandraPV <alisandra.pv@gmail.com>
2016-05-28 11:52:44 +0300

    Update main.c

--- courses/prog_base_2/tests/test_2/main.c
+++ courses/prog_base_2/tests/test_2/main.c
@@ -8,12 +8,12 @@
 
 void server_info(socket_t* clientSocket)
 {
-    cJSON * SM = cJSON_CreateObject();
-    cJSON_AddItemToObject(SM, "student", cJSON_CreateString("Pyvovarchuk Olexandra"));
-    cJSON_AddItemToObject(SM, "group", cJSON_CreateString("KP-51"));
-    cJSON_AddItemToObject(SM, "variant", cJSON_CreateNumber(17));
-    char * jsonSM = cJSON_Print(SM);
-    server_sent(clientSocket,jsonSM);
+    cJSON * stud = cJSON_CreateObject();
+    cJSON_AddItemToObject(stud, "student", cJSON_CreateString("Pyvovarchuk Olexandra"));
+    cJSON_AddItemToObject(stud, "group", cJSON_CreateString("KP-51"));
+    cJSON_AddItemToObject(stud, "variant", cJSON_CreateNumber(17));
+    char * jsonstud = cJSON_Print(stud);
+    server_sent(clientSocket,jsonstud);
 }
 
 void server_sent(socket_t* clientSocket, char* text)
@@ -23,6 +23,6 @@ void server_sent(socket_t* clientSocket, char* text)
             "Content-Type: application/json\n"
             "Content-Length: %i\r\n\r\n"
             "%s\n",strlen(text),text);
-    socket_write_string(clientSocket,buf); 
+    socket_write_string(clientSocket,buf);
 }
 
