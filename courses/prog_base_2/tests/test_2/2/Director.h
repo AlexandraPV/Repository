269ae86eeebd7cef8b8806900c491c81fa962e27
AlexandraPV <alisandra.pv@gmail.com>
2016-05-28 13:56:03 +0300

    Create Director.h

---
+++ courses/prog_base_2/tests/test_2/2/Director.h
@@ -0,0 +1,17 @@
+
+typedef struct Director direct;
+
+direct * Director_new(void);
+void Director_free(direct * self);
+void Director_print(direct * self);
+
+void Director_set(direct * self, char * name, char * surname, char * birthdate, char * company,char * name_com, char * specialization, int year,double rating);
+
+char * Director_name_get(direct * self);
+char * Director_surname_get(direct * self);
+char * Director_birthdate_get(direct * self);
+char * Director_company_get(direct * self);
+char * Director_name_com_get(direct * self);
+char * Director_specialization_get(direct * self);
+int Director_year_get(direct * self);
+double Director_rating_get(direct * self);
