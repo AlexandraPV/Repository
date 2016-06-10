89695bfe592d7f5d940bf8c59cdbc3f8ab5f11cc
AlexandraPV <alisandra.pv@gmail.com>
2016-06-10 23:33:06 +0300

    Create SliderPuzzleAppDelegate.h

---
+++ courses/prog_base_3/project/conclusion/puzzle/SliderPuzzleAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SliderPuzzleAppDelegate.h
+//  Slider Puzzle
+//
+//  Created by Alexandra on 21.05.16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SliderPuzzleMainViewController;
+
+@interface SliderPuzzleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SliderPuzzleMainViewController *mainViewController;
+
+@end
