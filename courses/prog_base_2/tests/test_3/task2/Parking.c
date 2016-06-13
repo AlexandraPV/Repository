d719b09b5ad66d6e0a08cae74be08bcbd72f23e6
AlexandraPV <alisandra.pv@gmail.com>
2016-06-13 14:13:16 +0300

    Add files via upload

--- courses/prog_base_2/tests/test_3/task2/Parking.c
+++ courses/prog_base_2/tests/test_3/task2/Parking.c
@@ -1,55 +1,104 @@
+//
+//  Parking.c
+//  Parking
+//
+//  Created by Alexandra on 13.06.16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
 #include "Parking.h"
-#include <stdbool.h>
-#include <stdio.h>
+#include "list.h"
 #include <stdlib.h>
-#include <strings.h>
-#include <stdbool.h>
 #include <string.h>
 
-
-struct B{
-    char *name;
-    char *timeIn;
-    char *timeFrom;
-    int numberOFcar;
-    
-};
-
-struct A{
+typedef struct park_s
+{
     int count;
-    bool empty;
-    CAR all[MAX_COUNT_CAR];
-};
+    int free_count;
+    list_t history_in;
+    list_t history_out;
+    list_t cars;
+} park_t;
 
+typedef struct car_s
+{
+    int id;
+    char * name;
+} car_t;
 
-CAR * car_new(char * name, char * timeIN, int numberOFcar){
-    CAR * st = malloc(sizeof(struct A));
-    st->name = name;
-    st->timeIn=timeIN;
-    st->timeFrom = 0;
-    st->numberOFcar = numberOFcar;
-    return st;
-
-   
-}
-void car_free(CAR *car){
-    free(car);
-}
-
-PARKING * park_new(int count){
-    PARKING * st = malloc(sizeof(struct A));
-}
-void park_free(PARKING *park){
-    free(park);
+park_t * park_new(int count)
+{
+    park_t * park = malloc(sizeof(struct park_s));
+    park->count = count;
+    park->free_count = count;
+    park->history_in = list_new();
+    park->history_out = list_new();
+    park->cars = list_new();
+    return park;
 }
 
-int count_all_park(PARKING * park);
-int count_empty_park(PARKING * park);
+int park_in(park_t * p, car_t * car)
+{
+    if (p->free_count > 0)
+    {
+        list_add(p->history_in, car, 0);
+        list_add(p->cars, car, 0);
+        p->free_count--;
+        return 1;
+    }
+    return 0;
+}
 
-void enter_car(PARKING * park, CAR * car);
-void exit_car(PARKING * park, CAR * car);
+int park_out(park_t * p, car_t *car)
+{
+    for (int i = 0; i < list_size(p->cars); i++)
+        if (list_get(p->cars, i) == car)
+        {
+            list_add(p->history_out, car, 0);
+            list_del(p->cars, i);
+            p->free_count++;
+            return 1;
+        }
+    return 0;
+}
 
-void list_of_car(PARKING *park);
-void info_person(CAR * car);
+list_t park_get_cars(park_t * p)
+{
+    list_t list = list_new();
+    for (int i = 0; i < list_size(p->cars); i++)
+        list_add(list, list_get(p->cars, i), i);
+    return list;
+}
 
-void history_of_ener(PARKING * park, int N);
+list_t park_get_in_hist(park_t * p, int n)
+{
+    list_t list = list_new();
+    for (int i = 0; i < list_size(p->history_in) && i < n; i++)
+        list_add(list, list_get(p->history_in, i), i);
+    return list;
+}
+list_t park_get_out_hist(park_t * p, int n)
+{
+    list_t list = list_new();
+    for (int i = 0; i < list_size(p->history_out) && i < n; i++)
+        list_add(list, list_get(p->history_out, i), i);
+    return list;
+    
+}
+
+int park_get_all_pl(park_t * p)
+{
+    return p->count;
+}
+int park_get_free_pl(park_t * p)
+{
+    return p->free_count;
+}
+car_t * car_new(int id, char * name)
+{
+    car_t * car = malloc(sizeof(struct car_s));
+    car->id = id;
+    car->name = malloc(sizeof(char) * strlen(name));
+    strcpy(car->name, name);
+    return car;
+}
--- courses/prog_base_2/tests/test_3/task2/Parking.h
+++ courses/prog_base_2/tests/test_3/task2/Parking.h
@@ -1,6 +1,6 @@
 //
 //  Parking.h
-//  test#2
+//  Parking
 //
 //  Created by Alexandra on 13.06.16.
 //  Copyright © 2016 Alexandra. All rights reserved.
@@ -10,30 +10,19 @@
 #define Parking_h
 
 #include <stdio.h>
-#define MAX_COUNT_CAR 20
+#include "list.h"
+typedef struct park_s park_t;
+typedef struct car_s car_t;
 
-typedef struct A PARKING;
-typedef struct B CAR;
-
-CAR * car_new(char * namem, char * timeIN,  int numberOFcar);
-void car_free(CAR *car);
-
-PARKING * park_new(int count);
-void park_free(PARKING *park);
-
-int count_all_park(PARKING * park);
-int count_empty_park(PARKING * park);
-
-void enter_car(PARKING * park, CAR * car);
-void exit_car(PARKING * park, CAR * car);
-
-void list_of_car(PARKING *park);
-void info_person(CAR * car);
-
-void history_of_ener(PARKING * park, int N);
-
-
-
+park_t * park_new(int count);
+int park_in(park_t * p, car_t * car);
+int park_out(park_t * p, car_t *car);
+list_t park_get_cars(park_t * p);
+list_t park_get_in_hist(park_t * p, int n);
+list_t park_get_out_hist(park_t * p, int n);
+int park_get_all_pl(park_t * p);
+int park_get_free_pl(park_t * p);
+car_t * car_new(int id, char * name);
 
 
 
